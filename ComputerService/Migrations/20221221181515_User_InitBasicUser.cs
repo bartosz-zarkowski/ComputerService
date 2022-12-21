c0d48b541fa5ddff9aaa8e68961161d19f869899
bartosz-zarkowski <bartoszzarkowski@gmail.com>
2022-12-21 19:19:25 +0100

    Init basic user

---
+++ ComputerService/Migrations/20221221181515_User_InitBasicUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserInitBasicUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "IF NOT EXISTS (SELECT Id FROM Users WHERE Id = '683a9aa8-3784-4e0f-a392-0b2dbd3b592f')"
+                + "INSERT INTO Users (Id,CreatedAt,FirstName,LastName,Email,Password, PhoneNumber, IsActive, Role, Salt)"
+                + $"VALUES ('683a9aa8-3784-4e0f-a392-0b2dbd3b592f', '{DateTimeOffset.Now}', 'User', 'Administrator', 'admin', 'LlB2nDpZKpI6jfM6n0HEOIet+IDxu0P9dpLgR1MqpmCxGt2S', '756456783', 'true', 'Administrator', 'LlB2nDpZKpI6jfM6n0HEOA==')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
