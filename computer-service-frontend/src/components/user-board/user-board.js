7f5316eb4322e99712c723aefbfc9fbe5136540d
bartosz-zarkowski <bartoszzarkowski@gmail.com>
2023-01-02 11:12:04 +0100

    Add footer and app version

--- computer-service-frontend/src/components/user-board/user-board.js
+++ computer-service-frontend/src/components/user-board/user-board.js
@@ -18,6 +18,8 @@ import {
 } from "react-bootstrap-icons";
 import AuthVerify from "../../services/auth/auth-verify";
 import Topbar from "./topbar";
+import Footer from "./footer";
+import { Row } from "react-bootstrap";
 
 const UserBoard = () => {
   const [showReceiverBoard, setShowReceiverBoard] = useState(false);
@@ -147,9 +149,15 @@ const UserBoard = () => {
                 </div>
               </Link>
             )}
+            <div className="app-version">
+              App version: {process.env.REACT_APP_VERSION}
+            </div>
           </div>
         </div>
+        <div className="content">
           <AppRoutes />
+          <Footer />
+        </div>
         <AuthVerify logOut={logOut} />
       </div>
     </div>
